1c8f1df868028d2fc9e39027ca8bf879783113ee
Alvis <weiwendy0430@gmail.com>
2025-02-25 17:26:13 +0800

    修改設定檔

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vue-login/',
+  base: '/webPage/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
