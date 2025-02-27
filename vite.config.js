f5c9beb1416949f823d8003ee7cf30c92869ed63
Alvis <weiwendy0430@gmail.com>
2025-02-27 13:20:19 +0800

    修復部屬問題

--- vite.config.js
+++ vite.config.js
@@ -4,26 +4,24 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-export default defineConfig(({ command }) => {
-  const isProduction = command === 'build'
-  return {
-    base: isProduction ? '/webPage/' : '/',
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor'
-            }
-          },
+// 使用固定的base路径，不要根据环境切换
+export default defineConfig({
+  base: '/webPage/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
         },
       },
     },
-    plugins: [vue(), vueDevTools()],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
-      },
+  },
+  plugins: [vue(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  }
+  },
 })
